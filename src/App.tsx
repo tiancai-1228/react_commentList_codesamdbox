2a0df44cebe72dc50ef49f471bc8e70f8e8d2499
tiancai <49026573+tiancai-1228@users.noreply.github.com>
2022-05-23 12:23:32 +0800

    message list message list

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,40 @@
 import "./styles.css";
+import React, { useState, useEffect } from "react";
 
 export default function App() {
+  const [messageList, setMessageList] = useState<string[]>([]);
+  // const form = document.forms[0];
+
+  const handleMessage = () => {
+    const message = document.forms[0].myMessage.value;
+    if (message) {
+      const messageAry = [...messageList, message];
+      setMessageList(messageAry);
+      localStorage.setItem("messageList", messageAry.join());
+    }
+  };
+
+  useEffect(() => {
+    const list = localStorage.getItem("messageList");
+    if (list) {
+      setMessageList(list.split(","));
+    }
+  }, []);
   return (
     <div className="App">
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>leave a message demo app</h1>
+      <form className="input">
+        <textarea name="myMessage" rows={4} cols={60} required></textarea>
+        <button
+          type="button"
+          className="submitBtn"
+          onClick={() => {
+            handleMessage();
+          }}
+        >
+          submit
+        </button>
+      </form>
     </div>
   );
 }
